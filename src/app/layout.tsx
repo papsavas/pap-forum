9a4a392cf4ce5c07a11c2b8610a9f46950b196df
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-05-29 22:48:58 +0300

    feat: Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,25 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google';
+import Navbar from '../components/Navbar';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Forum",
-  description: "A forum template",
+	title: 'Forum',
+	description: 'A forum template',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${inter.className} bg-neutral-950`}>
+				<Navbar endpoints={['Threads', 'Profile']} />
+				{children}
+			</body>
+		</html>
+	);
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { FC } from 'react';
+
+const Navbar: FC<{ endpoints: string[] }> = ({ endpoints }) => {
+	return (
+		<nav className="bg-black px-8 py-3">
+			<div className="flex justify-start gap-10 ">
+				{endpoints.map((e) => (
+					<Link
+						className="text-neutral-200 transition-colors duration-500 hover:text-white"
+						key={e}
+						href={e}
+					>
+						{e}
+					</Link>
+				))}
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
