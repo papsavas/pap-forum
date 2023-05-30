0337d1fc45000f45748990336014c6b0093dc305
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-05-30 23:08:24 +0300

    feat: posts page

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import Post from '../../components/Post';
+import { getPosts } from '../../lib/api';
+
+export default async function PostsPage() {
+	const posts = await getPosts();
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between gap-10">
+			{posts.map((p) => (
+				<Link href={`posts/${p.id}`} key={p.id}>
+					<Post
+						{...p}
+						key={p.id}
+						root
+						className="transition-colors duration-100 hover:bg-neutral-600"
+					/>
+				</Link>
+			))}
+		</main>
+	);
+}
