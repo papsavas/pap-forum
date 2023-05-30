5a44010d973fed7ece5a683943bb6a6cf2d43921
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-05-30 23:08:08 +0300

    feat: Post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,15 +1,65 @@
-import { Post, getUser } from '../lib/api';
+import { FC } from 'react';
+import { twMerge } from 'tailwind-merge';
+import { Comment, getUser } from '../lib/api';
 import User from './User';
 
-export default async function Post({ title, userId, body }: Post) {
+//@ts-expect-error async fc
+const Post: FC<
+	{
+		id: string | number;
+		userId: User['id'];
+		title: string;
+		body: string;
+		comments?: Comment[];
+		className?: string;
+	} & XOR<{ root: boolean }, { parentId: string }>
+> = async ({
+	title,
+	userId,
+	body,
+	className = '',
+	comments,
+	root,
+	parentId,
+}) => {
 	const user = await getUser(userId);
+	const hasComments = comments && comments.length > 0;
 	return (
-		<article className="flex-flex-col h-full w-full flex-1 gap-8 rounded-xl bg-neutral-700 p-4 text-white">
-			<User {...user} />
-			<div className="ml-4 mt-2">
-				<h1>{title}</h1>
-				<p className="">{body}</p>
+		<section
+			className={twMerge(
+				`flex-flex-col h-full w-full flex-1 
+				justify-evenly gap-8 overflow-auto rounded-xl
+				bg-neutral-700 text-white`,
+				className
+			)}
+		>
+			<div className={`p-4 ${parentId ? 'border-t border-opacity-60' : ''}`}>
+				<div className={`${root ? 'mb-12' : 'mb-2'} flex flex-col gap-4`}>
+					<User {...user} size={root ? 50 : 35} />
+					<article className={`flex flex-col gap-2`}>
+						<h1 className="text-lg">{title}</h1>
+						<p className="">{body}</p>
+					</article>
+				</div>
+				<div className="">
+					<div className="ml-3 flex flex-col text-sm">
+						{hasComments
+							? comments.map((c) => (
+									<Post
+										id={c.id}
+										parentId={c.postId.toString()}
+										body={c.body}
+										title={c.name}
+										key={c.id}
+										userId={c.email}
+									/>
+							  ))
+							: null}
+					</div>
+				</div>
 			</div>
-		</article>
+		</section>
 	);
-}
+};
+
+export default Post;
--- src/index.d.ts
+++ src/index.d.ts
@@ -2,3 +2,13 @@ type SegmentProps = {
   params: { id: string };
   searchParams: { [key: string]: string | string[] | undefined };
 };
+
+type XOR<T1, T2> =
+  | ({
+    [K in keyof T1]: T1[K] }
+    & { [K in keyof T2]?: never
+    })
+  | ({
+    [K in keyof T2]: T2[K] }
+    & { [K in keyof T1]?: never
+    })
