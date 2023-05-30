975d73f7ce2378393773293f4807f899b5af6a29
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-05-30 23:04:55 +0300

    refactor: cleanup home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
-import Post from '../components/Post';
 import { getPosts } from '../lib/api';
 
 export default async function Home() {
 	const posts = await getPosts();
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between gap-10 p-24">
-			{posts.map((p) => (
-				//@ts-expect-error server component
-				<Post {...p} key={p.id} />
-			))}
-		</main>
-	);
+	return <h1 className="text-7xl">Home</h1>;
 }
