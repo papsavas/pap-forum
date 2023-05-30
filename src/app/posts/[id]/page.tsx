d50d7a13c623b1bc0f76a0c932441a119b4d4571
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-05-30 23:08:39 +0300

    feat: post page

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,12 @@
+import Post from '../../../components/Post';
+import { getPost, getPostComments } from '../../../lib/api';
+
+export default async function PostPage({ params }: SegmentProps) {
+	const comments = await getPostComments(params.id);
+	const post = await getPost(params.id);
+	return (
+		<article className="flex h-full w-full flex-1 flex-col">
+			<Post {...post} root comments={comments} />
+		</article>
+	);
+}
