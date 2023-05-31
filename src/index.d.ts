e2a4e2b51b0e1a6c6c1959ff3437105732e63e2a
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-05-31 17:21:34 +0300

    chore: format code

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,14 +1,18 @@
 type SegmentProps = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
+	params: { id: string };
+	searchParams: { [key: string]: string | string[] | undefined };
 };
 
 type XOR<T1, T2> =
-  | ({
-    [K in keyof T1]: T1[K] }
-    & { [K in keyof T2]?: never
-    })
-  | ({
-    [K in keyof T2]: T2[K] }
-    & { [K in keyof T1]?: never
-    })
+	| ({
+			[K in keyof T1]: T1[K];
+	  } & {
+			[K in keyof T2]?: never;
+	  })
+	| ({
+			[K in keyof T2]: T2[K];
+	  } & {
+			[K in keyof T1]?: never;
+	  });
+
+type Nullable<T> = { [K in keyof T]: T[K] | null };
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,49 +1,53 @@
-const BASE = 'https://jsonplaceholder.typicode.com'
+const BASE = 'https://jsonplaceholder.typicode.com';
 
 export type Post = {
-  userId: User['id'],
-  id: string | number,
-  title: string,
-  body: string
-}
+	userId: User['id'];
+	id: string | number;
+	title: string;
+	body: string;
+};
 
 export type Comment = {
-  postId: number,
-  id: string | number,
-  name: string,
-  email: string,
-  body: string
-}
+	postId: number;
+	id: string | number;
+	name: string;
+	email: string;
+	body: string;
+};
 
 export type User = {
-  id: number | string;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
-}
+	id: number | string;
+	name: string;
+	username: string;
+	email: string;
+	address: {
+		street: string;
+		suite: string;
+		city: string;
+		zipcode: string;
+		geo: {
+			lat: string;
+			lng: string;
+		};
+	};
+	phone: string;
+	website: string;
+	company: {
+		name: string;
+		catchPhrase: string;
+		bs: string;
+	};
+};
 
-const fetcher = (endpoint: string) => fetch(`${BASE}/${endpoint}`).then(r => r.json())
+const fetcher = (endpoint: string) =>
+	fetch(`${BASE}/${endpoint}`).then((r) => r.json());
 
-export const getPost = (id: Post['id']) => fetcher(`posts/${id}`) as Promise<Post>;
+export const getPost = (id: Post['id']) =>
+	fetcher(`posts/${id}`) as Promise<Post>;
 export const getPosts = () => fetcher(`posts`) as Promise<Post[]>;
 export const getUsers = () => fetcher(`users`) as Promise<User[]>;
-export const getUser = (id: User['id']) => fetcher(`users/${id}`) as Promise<User>;
+export const getUser = (id: User['id']) =>
+	fetcher(`users/${id}`) as Promise<User>;
 export const getComments = () => fetcher(`comments`) as Promise<Comment[]>;
-export const getPostComments = (postId: Post['id']) => fetcher(`posts/${postId}/comments`) as Promise<Comment[]>;
+export const getPostComments = (postId: Post['id']) =>
+	fetcher(`posts/${postId}/comments`) as Promise<Comment[]>;
