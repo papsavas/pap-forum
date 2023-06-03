263b1c88818c63223c775af86ad13c9f5b57dadd
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-06-03 23:59:01 +0300

    types(db): QueryOptions

--- src/index.d.ts
+++ src/index.d.ts
@@ -5,14 +5,20 @@ type SegmentProps = {
 
 type XOR<T1, T2> =
 	| ({
-			[K in keyof T1]: T1[K];
-	  } & {
+		[K in keyof T1]: T1[K];
+	} & {
 			[K in keyof T2]?: never;
-	  })
+		})
 	| ({
-			[K in keyof T2]: T2[K];
-	  } & {
+		[K in keyof T2]: T2[K];
+	} & {
 			[K in keyof T1]?: never;
-	  });
+		});
 
 type Nullable<T> = { [K in keyof T]: T[K] | null };
+
+type QueryOptions<T> = {
+	columns?: { [K in keyof Partial<T>]: boolean },
+	limit?: number,
+	offset?: number
+}
