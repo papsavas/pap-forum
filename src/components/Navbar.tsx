71b046f33898c474e2e6c7b3f97f8eb86782d4c5
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-06-03 14:10:44 +0300

    fix(Navbar): accept object paths

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,28 @@
 import Link from 'next/link';
 import { FC } from 'react';
 
-const Navbar: FC<{ endpoints: string[] }> = ({ endpoints }) => {
+const Navbar: FC<{
+	endpoints: (string | { name: string; path: string })[];
+}> = ({ endpoints }) => {
 	return (
 		<nav className="bg-black px-8 py-3">
 			<div className="flex justify-start gap-10 ">
-				{endpoints.map((e) => (
-					<Link
-						className="text-neutral-200 transition-colors duration-500 hover:text-white"
-						key={e}
-						href={e[0].toLowerCase() + e.slice(1)}
-					>
-						{e}
-					</Link>
-				))}
+				{endpoints.map((e) => {
+					const [path, name] =
+						typeof e === 'object'
+							? [e.path, e.name]
+							: [e[0].toLowerCase() + e.slice(1), e];
+
+					return (
+						<Link
+							className="text-neutral-200 transition-colors duration-500 hover:text-white"
+							key={path}
+							href={path}
+						>
+							{name}
+						</Link>
+					);
+				})}
 			</div>
 		</nav>
 	);
