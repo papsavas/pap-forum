c2a52586ea23a06348984e72175e5e01b2c136d1
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-06-08 20:30:23 +0300

    feat(Table): Layout

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,8 +1,17 @@
 import { Row, Table, flexRender } from '@tanstack/react-table';
+import { useId } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-type Props<T> = {
-	table: Table<T>;
+type Props<T> = XOR<
+	{ table: Table<T> },
+	{
+		layout: {
+			headers?: string[];
+			rows: number;
+			cols: number;
+		};
+	}
+> & {
 	onRowClick?: (row: Row<T>) => unknown;
 	styles?: Partial<{
 		table: string;
@@ -15,33 +24,70 @@ type Props<T> = {
 	}>;
 };
 
-export default function Table<T>({ table, styles, onRowClick }: Props<T>) {
+export default function Table<T>({
+	table,
+	layout,
+	styles,
+	onRowClick,
+}: Props<T>) {
+	const headerGroups = table ? table.getHeaderGroups() : [layout.headers];
+	const rows = table
+		? table.getRowModel().rows
+		: Array<null>(layout.rows).fill(null);
+	const id = useId();
 	return (
 		<table className={twMerge('table-auto text-center', styles?.table)}>
 			<thead className={twMerge('', styles?.head)}>
-				{table.getHeaderGroups().map((headerGroup) => (
-					<tr key={headerGroup.id} className={twMerge('', styles?.headRow)}>
-						{headerGroup.headers.map((header) => (
-							<th key={header.id} className={twMerge('', styles?.headCell)}>
-								{flexRender(
-									header.column.columnDef.header,
-									header.getContext()
-								)}
-							</th>
-						))}
-					</tr>
-				))}
+				{headerGroups.map((headerGroup, hgi) =>
+					headerGroup ? (
+						<tr
+							key={
+								'length' in headerGroup
+									? `${id}_head_group_${hgi}`
+									: headerGroup.id
+							}
+							className={twMerge('', styles?.headRow)}
+						>
+							{('length' in headerGroup
+								? headerGroup
+								: headerGroup.headers
+							).map((header, hi) => (
+								<th
+									key={
+										typeof header === 'string' ? `${id}_head_${hi}` : header.id
+									}
+									className={twMerge('', styles?.headCell)}
+								>
+									{typeof header === 'string'
+										? header
+										: flexRender(
+												header.column.columnDef.header,
+												header.getContext()
+										  )}
+								</th>
+							))}
+						</tr>
+					) : null
+				)}
 			</thead>
+
 			<tbody className={twMerge('', styles?.body)}>
-				{table.getRowModel().rows.map((row) => (
+				{rows.map((row, ri) => (
 					<tr
-						key={row.id}
+						key={row?.id ?? `${id}_body_row_${ri}`}
 						className={twMerge('', styles?.bodyRow)}
-						onClick={(e) => onRowClick && onRowClick(row)}
+						onClick={(e) => onRowClick && row && onRowClick(row)}
 					>
-						{row.getVisibleCells().map((cell) => (
-							<td key={cell.id} className={twMerge('', styles?.cell)}>
-								{flexRender(cell.column.columnDef.cell, cell.getContext())}
+						{(
+							row?.getVisibleCells() ?? Array<null>(layout!.cols).fill(null)
+						).map((cell, ci) => (
+							<td
+								key={cell?.id ?? `${id}_row_${ri}_cell_${ci}`}
+								className={twMerge('', styles?.cell)}
+							>
+								{cell
+									? flexRender(cell.column.columnDef.cell, cell.getContext())
+									: null}
 							</td>
 						))}
 					</tr>
